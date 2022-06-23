7c6f95235c1f861ad1387e2a9e834d025c7a94f3
Oulol <97284462+Oulol@users.noreply.github.com>
2022-06-23 19:05:18 +0300

    Create main.lua

---
+++ amogus/main.lua
@@ -0,0 +1,32 @@
+-- Initialaizing Script
+wait(2)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+	Title = "Amogus Script v1.2";
+	Text = "Script was initialized sucessfuly!";
+	Duration = "7";
+})
+local char = game:GetService("Players").LocalPlayer.Character
+local anim = char:WaitForChild("Animate")
+-- Add fuction
+function change(anim,id)
+	local chlds = anim:GetChildren()
+	for i=1,#chlds do
+		chlds[i].AnimationId = id
+	end
+end
+-- Change animations
+change(anim.idle,"rbxassetid://9992387403")
+change(anim.jump,"rbxassetid://9992799603")
+change(anim.fall,"rbxassetid://9992799603")
+change(anim.walk,"rbxassetid://9993013322")
+change(anim.run,"rbxassetid://9993013322")
+change(anim.dance,"rbxassetid://9993118595")
+change(anim.dance2,"rbxassetid://9993211479")
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+	Title = "Amogus Script v1.2";
+	Text = "Animations was changed sucessfuly! To appy them you need to jump.";
+	Duration = "7";
+})
+
+script:Destroy()
